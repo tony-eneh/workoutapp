7e7a7b1ec1fecebb7a538c7aa47f0cf3e7cade25
Tony Eneh <aeneh90@gmail.com>
2020-11-29 10:48:33 +0100

    Feat: graphical chart on dashboard

--- screens/dashboard.js
+++ screens/dashboard.js
@@ -1,9 +1,20 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {StyleSheet, View} from 'react-native';
 import Header from '../components/Header';
 import EmptyNotice from '../components/NoWorkoutYetNotice';
+import {AnimatedCircularProgress} from 'react-native-circular-progress';
+import {Text, Block, Button} from 'galio-framework';
+import theme from '../constants/Theme';
+import Avatar from '../assets/Avatar.png';
 
-let workouts;
+// static app states
+// TODO use redux to manage state
+let workouts = true;
+const profile = {
+  avatar: Avatar,
+  name: 'Tony Eneh',
+  email: 'tony.eneh@kmail.com',
+};
 
 export default function Dashboard({navigation, route}) {
   return (
@@ -12,8 +23,67 @@ export default function Dashboard({navigation, route}) {
       {!workouts ? (
         <EmptyNotice navigation={navigation} />
       ) : (
-        <Text>Dashboard Screen</Text>
+        <Block style={[styles.container, styles.center]}>
+          <Text>
+            Welcome{' '}
+            <Text p color={theme.COLORS.ACTIVE}>
+              {profile.name}
+            </Text>
+          </Text>
+          <Text h4>Last Workout Stats</Text>
+          <Block style={[styles.timeBlock, styles.center]}>
+            <AnimatedCircularProgress
+              size={120}
+              width={15}
+              fill={90}
+              tintColor={theme.COLORS.ACTIVE}
+              onAnimationComplete={() => console.log('onAnimationComplete')}
+              backgroundColor={theme.COLORS.MUTED}
+              rotation={'360'}
+              children={(fill) => <Percent fill={fill} label={'TIME GOAL'} />}
+            />
+            <Text>TOTAL JOGGING TIME {'2hrs 5mins'}</Text>
+          </Block>
+          <Block style={[styles.distanceBlock, styles.center]}>
+            <AnimatedCircularProgress
+              size={120}
+              width={15}
+              fill={55}
+              tintColor={theme.COLORS.ACTIVE}
+              onAnimationComplete={() => console.log('onAnimationComplete')}
+              backgroundColor={theme.COLORS.MUTED}
+              rotation={'360'}
+              children={(fill) => <Percent fill={fill} label={'TIME GOAL'} />}
+            />
+            <Text>TOTAL DISTANCE COVERED {'14km'}</Text>
+          </Block>
+          <Button
+            size="large"
+            color={theme.COLORS.BUTTON_COLOR}
+            onPress={() => navigation.navigate('WorkoutSession')}>
+            START JOGGING
+          </Button>
+        </Block>
       )}
     </>
   );
 }
+
+function Percent({fill, label}) {
+  return (
+    <Block center>
+      <Text h5>{fill}%</Text>
+      <Text>{label}</Text>
+    </Block>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {flex: 1},
+  center: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  timeBlock: {marginTop: 20},
+  distanceBlock: {marginVertical: 20},
+});
